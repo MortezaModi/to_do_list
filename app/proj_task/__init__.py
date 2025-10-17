0451f62e33a96cb36e3fb49a408e0b3bad7a674d
Morteza <maddahmasoud@gmail.com>
2025-10-17 19:23:14 +0330

    test

--- app/proj_task/__init__.py
+++ app/proj_task/__init__.py
@@ -1,2 +1,3 @@
-from app.proj_task.projects import Projects
-from app.proj_task.tasks import tasks
+from app.proj_task.project_task import Project, Task
+from app.proj_task.crud import manager
+from app.proj_task.config import MAX_PROJECTS, MAX_TASKS, VALID_STATUSES
