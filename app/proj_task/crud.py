9d47b1bdd65e0ea7a442023b66d52d8fc26ca974
Morteza <maddahmasoud@gmail.com>
2025-10-17 17:14:46 +0330

    create: class manager in crud.py
    checking for length of projects and despritions then returning the id title and description

--- app/proj_task/crud.py
+++ app/proj_task/crud.py
@@ -5,3 +5,19 @@ from datetime import datetime
 
 class manager:
     def __init__(self):
+        self.projects: list[Projects] = []
+
+    def create_project(self, title: str,description: str = "" ):
+        if len(title.split()) > 35:
+            raise ValueError("Title is longer than 35 words")
+        if len(description.split()) > 150:
+            raise ValueError("Description is longer than 150 words")
+        if any(p.title == title for p in self.projects):
+            raise ValueError("Title already exists")
+        if len(self.projects) >= MAX_PROJECTS:
+            raise ValueError("NO MORE PROJECTS ALLOWED TO BE MADE")
+
+    project = Projects(id=len(self.projects)+1, title=title, description=description)
+    self.projects.append(project)
+    return project
+
