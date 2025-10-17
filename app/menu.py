21b176a0cfa2634cdeb19f8ebc2a2047edc157f5
Morteza <maddahmasoud@gmail.com>
2025-10-17 18:27:36 +0330

    create : menu
    added choices to the menu

---
+++ app/menu.py
@@ -0,0 +1,25 @@
+from csv import excel
+
+from app.proj_task.crud import manager
+
+def run_menu() :
+    todo = manager()
+    while True :
+        print("\n  To Do list menu:")
+        print("1  Create new project")
+        print("2  Update projects")
+        print("3  Delete projects")
+        print("4  List projects")
+        print("5  Create tasks")
+        print("6  Update tasks")
+        print("7  Delete tasks")
+        print("8  Change task status")
+        print("9  List tasks")
+        print("0  Exit")
+
+        choice = int(input("Enter your choice: "))
+
+        try:
+            
+
+        except Exception as e :
