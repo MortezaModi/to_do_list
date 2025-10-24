8d2f7b66640a2d45373d2ee5cd23895f55fff46d
Morteza <maddahmasoud@gmail.com>
2025-10-24 10:13:22 +0330

    Update project_task.py
    the functions __repr__ and __str__ where defined in the wrong classes.
    
    the functions where defined in task and just have been moved to projects.

--- app/proj_task/project_task.py
+++ app/proj_task/project_task.py
@@ -9,6 +9,13 @@ class Project:
         self.description = description
         self.tasks: list[Task] = []
 
+         def __repr__(self):
+            return f"Project(id={self.id}, name='{self.name}')"
+
+        def __str__(self):
+            return f" {self.id}. {self.name} — {self.description}"
+
+
 
 class Task:
     def __init__(self, id: int, title: str, description: str = "", status: str = "todo", deadline: datetime | None = None):
@@ -26,8 +33,4 @@ class Task:
             deadline_str = self.deadline.strftime("%Y-%m-%d")
         return f"📝 {self.id}. {self.title} — {self.status} ({deadline_str})"
 
-    def __repr__(self):
-        return f"Project(id={self.id}, name='{self.name}')"
-
-    def __str__(self):
-        return f" {self.id}. {self.name} — {self.description}"
+   
